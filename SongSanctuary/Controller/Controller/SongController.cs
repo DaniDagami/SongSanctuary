9779420f1186d6ad2d70f12b0768d369a0d84c6a
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-03-21 20:10:29 +0200

    added most of the logic for SongController and Display

--- SongSanctuary/Controller/Controller/SongController.cs
+++ SongSanctuary/Controller/Controller/SongController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data;
+using SongSanctuary.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +8,44 @@ using System.Threading.Tasks;
 
 namespace Controller.Controller {
     public class SongController {
+        private ApplicationDbContext _appContext;
+        public List<Song> GetAll() {
+            using (_appContext = new ApplicationDbContext()) {
+                return _appContext.Songs.ToList();
+            }
+        }
+
+        public Song Get(int id) {
+            using (_appContext = new ApplicationDbContext()) {
+                return _appContext.Songs.Find(id);
+            }
+        }
+
+        public void Add(Song song) {
+            using (_appContext = new ApplicationDbContext()) {
+                _appContext.Songs.Add(song);
+                _appContext.SaveChanges();
+            }
+        }
+
+        public void Update(Song song) {
+            using (_appContext = new ApplicationDbContext()) {
+                var item = _appContext.Songs.Find(song.Id);
+                if (item != null) {
+                    _appContext.Entry(item).CurrentValues.SetValues(song);
+                    _appContext.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id) {
+            using (_appContext = new ApplicationDbContext()) {
+                var product = _appContext.Songs.Find(id);
+                if (product != null) {
+                    _appContext.Songs.Remove(product);
+                    _appContext.SaveChanges();
+                }
+            }
+        }
     }
 }
--- SongSanctuary/SongSanctuary/View/Display.cs
+++ SongSanctuary/SongSanctuary/View/Display.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Controller.Controller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,88 @@ using System.Threading.Tasks;
 
 namespace SongSanctuary.View {
     public class Display {
+        private int _closeOperationId = 6;
+        private SongController _songController = new SongController();
+
+        public Display() {
+            Input();
+        }
+
+        private void Input() {
+            var operation = -1;
+            do {
+                ShowMenu();
+                operation = int.Parse(Console.ReadLine());
+                switch (operation) {
+                    case 1:
+                        ListAll();
+                        break;
+                    case 2:
+                        Add();
+                        break;
+                    case 3:
+                        //Update();
+                        break;
+                    case 4:
+                        //Fetch();
+                        break;
+                    case 5:
+                        Delete();
+                        break;
+                    default:
+                        break;
+
+                }
+            } while (operation != _closeOperationId);
+        }
+
+        private void Delete() {
+            Console.WriteLine("Enter ID to delete: ");
+            int id = int.Parse(Console.ReadLine());
+            Song song = _songController.Get(id);
+            if (song == null)
+                Console.WriteLine("Song was not found!");
+
+            _songController.Delete(id);
+            Console.WriteLine("This song has been deleted!");
+        }
+
+        private void Add() {
+            Song song = new Song();
+            Console.WriteLine("Enter name: ");
+            song.Name = Console.ReadLine();
+            Console.WriteLine("Enter length: ");
+            song.Length = TimeSpan.Parse(Console.ReadLine());
+            Console.WriteLine("Enter genre: ");
+            song.Genre = Console.ReadLine();
+            Console.WriteLine("Do you want to add an albumId? Y for yes/ N for no: ");
+            string answer = Console.ReadLine();
+            if (answer.ToUpper() != "Y") { 
+                song.AlbumId = int.Parse(Console.ReadLine());
+            }
+            _songController.Add(song);
+        }
+
+        private void ListAll() {
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine(new string(' ', 16) + "PRODUCTS");
+            Console.WriteLine(new string('-', 40));
+            var songs = _songController.GetAll();
+            foreach (var song in songs) {
+                Console.WriteLine($"{song.Id} {song.Name} {song.Length} {song.Genre} {song.Album?.Name}");
+            }
+        }
+
+        private void ShowMenu() {
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine(new string(' ', 18) + "MENU");
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine("1. List all entries");
+            Console.WriteLine("2. Add new entry");
+            Console.WriteLine("3. Update entry");
+            Console.WriteLine("4. Fetch entry by ID");
+            Console.WriteLine("5. Delete entry by ID");
+            Console.WriteLine("6. Exit");
+        }
     }
 }
