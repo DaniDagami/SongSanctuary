168be1f053ba9582e9c4d6604166150a93132ef8
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-03-21 17:59:09 +0200

    made structure for Album and Artist

--- SongSanctuary/Data/Model/Album.cs
+++ SongSanctuary/Data/Model/Album.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SongSanctuary.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,13 @@ using System.Threading.Tasks;
 
 namespace Data.Model {
     public class Album {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseYear { get; set; }
+        public int SongCount => Songs.Count;
+        public int? BandId { get; set; }
+
+        public ICollection<Song> Songs { get; private set; }
+        public Band? Band { get; set; }
     }
 }
