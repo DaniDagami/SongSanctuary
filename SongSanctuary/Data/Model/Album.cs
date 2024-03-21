d30501d76dea0c994fd026f53e2a35bffc959aec
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-03-21 18:05:29 +0200

    Revert "made structure for Album and Artist"
    This reverts commit 168be1f053ba9582e9c4d6604166150a93132ef8.

--- SongSanctuary/Data/Model/Album.cs
+++ SongSanctuary/Data/Model/Album.cs
@@ -1,5 +1,4 @@
-﻿using SongSanctuary.View;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,13 +6,5 @@ using System.Threading.Tasks;
 
 namespace Data.Model {
     public class Album {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime ReleaseYear { get; set; }
-        public int SongCount => Songs.Count;
-        public int? BandId { get; set; }
-
-        public ICollection<Song> Songs { get; private set; }
-        public Band? Band { get; set; }
     }
 }
--- SongSanctuary/Data/Model/Artist.cs
+++ SongSanctuary/Data/Model/Artist.cs
@@ -6,12 +6,5 @@ using System.Threading.Tasks;
 
 namespace SongSanctuary.View {
     public class Artist {
-        public int Id { get; set; }
-        public string FullName { get; set; }
-        public string LastName { get; set; }
-        public bool Alive { get; set; }
-        public int? BandId { get; set; }
-
-        public Band? Band { get; set; }
     }
 }
