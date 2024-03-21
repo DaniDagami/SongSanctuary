615c21db9bcc344a78d1dba3873190dbc6649ab4
DaniDagami <daniel.g.boychev@gmail.com>
2024-03-21 17:59:33 +0200

    start

--- SongSanctuary/Data/Model/Band.cs
+++ SongSanctuary/Data/Model/Band.cs
@@ -6,5 +6,11 @@ using System.Threading.Tasks;
 
 namespace SongSanctuary.View {
     public class Band {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public int MemberCount { get; set; }
+
+        public ICollection<Artist> Artist { get; set;}
     }
 }
--- SongSanctuary/Data/Model/Song.cs
+++ SongSanctuary/Data/Model/Song.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,12 @@ using System.Threading.Tasks;
 
 namespace SongSanctuary.View {
     public class Song {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan Length { get; set; }
+        public string Genre { get; set; }
+
+        public int? AlbumId { get; set; }
+        public Album? Album { get; set; }
     }
 }
