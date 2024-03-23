cc02499a3559ab975f10f6c5e4cb479e66273ba4
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-03-24 00:15:37 +0200

    altered ShowCommands() in Display.cs

--- SongSanctuary/SongSanctuary/Views/Display.cs
+++ SongSanctuary/SongSanctuary/Views/Display.cs
@@ -8,16 +8,16 @@ namespace SongSanctuary.View {
     public abstract class Display {
         public static int closeOperationId = 6;
         public static void ShowCommands() {
-            Console.WriteLine(new string('-', 21));
-            Console.WriteLine(new string(' ', 6) + "Commands");
-            Console.WriteLine(new string('-', 21));
-            Console.WriteLine("1. List all entries");
-            Console.WriteLine("2. Add new entry");
-            Console.WriteLine("3. Update entry");
-            Console.WriteLine("4. Fetch entry by ID");
-            Console.WriteLine("5. Delete entry by ID");
-            Console.WriteLine("6. Return");
-            Console.WriteLine(new string('-', 21));
+            Console.WriteLine(new string('-', 31));
+            Console.WriteLine(new string(' ', 11) + "Commands");
+            Console.WriteLine(new string('-', 31));
+            Console.WriteLine(new string(' ', 5) + "1. List all entries");
+            Console.WriteLine(new string(' ', 5) + "2. Add new entry");
+            Console.WriteLine(new string(' ', 5) + "3. Update entry");
+            Console.WriteLine(new string(' ', 5) + "4. Fetch entry by ID");
+            Console.WriteLine(new string(' ', 5) + "5. Delete entry by ID");
+            Console.WriteLine(new string(' ', 5) + "6. Return");
+            Console.WriteLine(new string('-', 31));
         }
 
         protected static void ShowHeader(int maxCharacterLength, string info, string title) {
